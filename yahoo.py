1bd275606fa686618d8371eec895129e6bf7ae32
Thomas Wood <grand.edgemaster@gmail.com>
2019-10-24 01:45:08 +0100

    Split out get_best_photoinfo to utils file

--- yahoo.py
+++ yahoo.py
@@ -13,6 +13,7 @@ import math
 import logging
 
 import requests.exceptions
+from utils import get_best_photoinfo
 
 # number of seconds to wait before trying again
 HOLDOFF = 10
@@ -21,28 +22,6 @@ HOLDOFF = 10
 TRIES = 10
 
 hp = HTMLParser()
-
-
-def get_best_photoinfo(photoInfoArr, exclude=[]):
-    logger = logging.getLogger(name="get_best_photoinfo")
-    rs = {'tn': 0, 'sn': 1, 'hr': 2, 'or': 3}
-
-    # exclude types we're not interested in
-    for x in exclude:
-        if x in rs:
-            rs[x] = -1
-
-    best = photoInfoArr[0]
-    for info in photoInfoArr:
-        if info['photoType'] not in rs:
-            logger.error("photoType '%s' not known", info['photoType'])
-            continue
-        if rs[info['photoType']] >= rs[best['photoType']]:
-            best = info
-    if rs[best['photoType']] == -1:
-        return None
-    else:
-        return best
 
 
 def archive_email(yga, save=True, html=True):
